76dc0bf44f4f9822841672fb6d17b07924cb453f
sennedjem <sebastianariell@hotmail.com.ar>
2018-09-22 01:18:28 -0300

    posts utiliza ngx easy table y tiene filtros y sort

--- src/app/layout/posts/posts-routing.module.ts
+++ src/app/layout/posts/posts-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostsComponent } from './posts.component';
+import { TableModule } from 'ngx-easy-table';
 import { PostsGetAllResolver } from '../../shared/resolves/posts-get-all-resolver'
 
 const routes: Routes = [
@@ -11,7 +12,10 @@ const routes: Routes = [
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule],
+    exports: [
+    	RouterModule,
+        TableModule
+    ],
     providers: [PostsGetAllResolver]
 })
 export class PostsRoutingModule {
--- src/app/layout/posts/posts.component.ts
+++ src/app/layout/posts/posts.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { ActivatedRoute } from '@angular/router';
+import { ConfigService } from '../../shared/services';
 import { Post } from '../../shared/models';
 
 @Component({
     selector: 'app-posts',
     templateUrl: './posts.component.html',
     styleUrls: ['./posts.component.scss'],
-    animations: [routerTransition()]
+    animations: [routerTransition()],
+    providers: [ConfigService]
 })
 export class PostsComponent implements OnInit {
 
-   public data : Post[];
+   data : Post[];
+   configuration;
+   columns;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -21,7 +25,21 @@ export class PostsComponent implements OnInit {
     this.route.data.subscribe((data) => this.data = data.posts);
 
     this.data.map(function(post) {
-      return formatPost(post)});
+      return formatPost(post)
+    });
+
+    console.log(this.data)
+
+    this.columns = [
+            { key: 'id', title: 'Id', placeholder: 'Filtrar por Id'},
+            { key: 'content', title: 'content', placeholder: 'Filtrar por content' },
+            { key: 'url', title: 'Url', placeholder: 'Filtrar por url' },
+            { key: 'site', title: 'Site', placeholder: 'filtrar por site' },
+            { key: 'creation_date', title: 'Creation Date', placeholder: 'Creado en...' },
+            { key: 'tags', title: 'Tags', placeholder: 'Tags' }
+        ];
+
+     this.configuration = ConfigService.config;
 
     function formatPost(post){
       post.tags = post.tags.map(function(tag) {return tag.description});
