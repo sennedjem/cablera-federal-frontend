5a8e6720bab0ce3fdb63846f9c8ac1c154ad76b1
Dario Gutierrez <dariofg93@gmail.com>
2018-10-16 23:53:06 -0300

    intento heroku front

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+/**
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+//PathLocationStrategy
+
+app.get('/*', function(req,res) {
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log('Console listening!');
+ */
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,11 +23,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist/cablera-federal-frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/cablera-federal-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
