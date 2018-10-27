b1ce6407db5499a38aa33fbd03dfee80251492e5
Dario Gutierrez <dariofg93@gmail.com>
2018-10-27 20:54:12 -0300

    change back heroku name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,4 @@ export const environment = {
 };
 
 export const PATHFRONTEND = 'https://cablera-federal.herokuapp.com';
-export const PATHBACKEND = 'https://cablera-federal-backend.herokuapp.com/api';
+export const PATHBACKEND = 'https://cablera-federal-api.herokuapp.com/api';
