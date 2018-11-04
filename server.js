0241e4fc482970a0154f27dcc3b991f425f203e8
Dario Gutierrez <dariofg93@gmail.com>
2018-11-04 00:33:00 -0300

    nada

--- server.js
+++ server.js
@@ -13,22 +13,3 @@ app.get('/*', function(req,res) {
 })
 
 console.log('Console listening!');
-
-/*
- //Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cablera-federal-frontend'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/cablera-federal-frontend/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-*/
