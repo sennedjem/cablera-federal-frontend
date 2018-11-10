eee64e9ad2610ba0af3f6e79f95d8abd0ba84de1
Dario Gutierrez <dariofg93@gmail.com>
2018-11-10 02:46:38 -0300

    que ce tyo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-export const PATHFRONTEND = 'http://cablera-federal.herokuapp.com';
-export const PATHBACKEND = 'http://cablera-federal-api.herokuapp.com/api';
+export const PATHFRONTEND = 'https://cablera-federal.herokuapp.com';
+export const PATHBACKEND = 'https://cablera-federal-api.herokuapp.com/api';
